e3c7e264a3510069b13392fb49827dc913d97e12
Gael Gautho <gaelgautho07@gmail.com>
2025-07-14 08:44:35 +0200

    Profile Page

--- django_backend/post/api.py
+++ django_backend/post/api.py
@@ -2,8 +2,8 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, User
+from .serializers import PostSerializer, UserSerializer
 from .forms import PostForm
 
 
@@ -16,6 +16,20 @@ def post_list(request):
 
     return JsonResponse(serializer.data, safe=False)
 
+
+@api_view(['GET'])
+def post_list_profile(request, id):   
+    user = User.objects.get(pk=id)
+    posts = Post.objects.filter(created_by_id=id)
+
+    posts_serializer = PostSerializer(posts, many=True)
+    user_serializer = UserSerializer(user)
+
+    
+    return JsonResponse({
+        'posts': posts_serializer.data,
+        'user': user_serializer.data,
+    }, safe=False)
 
 
 @api_view(['POST'])
--- vue_frontend/src/App.vue
+++ vue_frontend/src/App.vue
@@ -36,7 +36,9 @@
                 <div class="menu-right">
                     
                     <template v-if="userStore.user.isAuthenticated && userStore.user.id">
+                        <RouterLink :to="{name: 'profile', params:{'id': userStore.user.id}}">
                             <img :src="userStore.user.avatar" class="w-12 rounded-full">
+                        </RouterLink>
                     </template>
 
                     <template v-else>
